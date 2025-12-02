463ffe77ed18a73e7b38980ffaffc791a60caa55
MEESAM RAZA <meesamrazaa512@gmail.com>
2025-12-02 12:01:09 +0500

    Add three Python function files

---
+++ greet_user.py
@@ -0,0 +1,5 @@
+def greet(name) : 
+  print("HELLO" , name )
+
+#EXAMPLE USE OF FUNCTION
+greet ("MEESAM RAZA")
---
+++ multiply.py
@@ -0,0 +1,6 @@
+def multiply(a,b) : 
+    return a + b
+
+
+#EXAMPLE USE OF FUNCTION
+print(multiply(4,6))
